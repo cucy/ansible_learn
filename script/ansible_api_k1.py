54b3e45d3a0c7b004e60c487dd874677a2f2d8fc
zrd <292016176@qq.com>
2018-03-18 10:06:27 +0800

    init

---
+++ script/ansible_api_k1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# _*_ coding:utf8 _*_ 
+
+# 核心class
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+from ansible.playbook.play import Play
+from ansible.executor.task_queue_manager import TaskQueueManager
+from ansible.plugins.callback import CallbackBase
